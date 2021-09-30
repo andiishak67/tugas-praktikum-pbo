e21f14e4b0de85eca016919404618c71da16a7ae
andiishak67 <91652502+andiishak67@users.noreply.github.com>
2021-09-30 13:58:59 +0800

    Add files via upload

---
+++ aritmatika.java
@@ -0,0 +1,27 @@
+class aritmatika{
+  public static void main (String[] args){
+  	
+  	   int aa=5;
+  	   int bb=6;
+  	   int cc=5;
+  	   int hasil;
+  	   
+  	   
+  	   hasil= aa + bb;
+  	   System.out.println(aa +" + "+ bb  +" = "+ hasil);
+  	   
+  	   hasil= aa * bb * cc;
+  	   System.out.printf(" %d * %d * %d = %d \n",aa,bb,cc,hasil);
+  	   
+  	   hasil= bb / cc;
+  	   System.out.printf(" %d / %d = %d \n",bb,cc,hasil);
+  	   
+
+   	   
+  	   
+  }
+}
+
+
+
+
---
+++ biodata.java
@@ -0,0 +1,9 @@
+public class biodata {	
+  public static void main(String[] args)  {
+  
+ 
+ System.out.println("saudara     : andi ishak");
+ System.out.println("tinggal di  : jl dr ratulangi");
+ System.out.println("berumur     : 19 tahun");
+ }
+}
